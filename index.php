2fee934f0eca91dc6bcbd10ecd613040b34da31c
Faizan Ahmed <faizanahmed497@gmail.com>
2022-12-05 14:41:36 +0500

    HTML5 SCSS Boilerplate v0.0.1

--- index.php
+++ index.php
@@ -1,85 +1,105 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <meta name="description" content="meta description">
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en" dir="ltr"> <!--<![endif]-->
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document Name | Product Name </title>
 
-   <title>Bakery</title>
+        <!-- Meta tags to be used for SEO purpose -->
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="author" content="MedCareMSO Development Team">
 
-   <!--=== Favicon ===-->
-   <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+        <!-- Meta tags to display cards whenever link is shared to any social platform or website -->
+        <!-- Open Graph Meta Data -->
+        <meta property="og:title" content="">
+        <meta property="og:type" content="">
+        <meta property="og:url" content="">
+        <meta property="og:description" content="">
+        <meta property="og:image" content="">
 
-   <!--== Google Fonts ==-->
-   <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,400i,500,600,700%7CPlayfair+Display:400,400i"
-         rel="stylesheet">
+        <meta name="twitter:card" content="">
+        <meta name="twitter:site" content="">
+        <meta name="twitter:title" content="">
+        <meta name="twitter:description" content="">
+        <meta name="twitter:image" content="">
 
+<<<<<<< Updated upstream:index.php
    <!--=== All Plugins CSS ===-->
    <link rel="stylesheet" type="text/css" href="resources/plugin/css/bootstrap.min.css">
+=======
+        <link rel="manifest" href="site.webmanifest">
+        <!-- Place favicon.ico in the root directory -->
+        <link rel="apple-touch-icon" href="imgs/favicon.png">
+        <link rel="icon" href="imgs/favicon.png" type="image/x-icon">
+        <link rel="icon" href="/favicon.ico" sizes="any">
+        <link rel="icon" href="/icon.svg" type="image/svg+xml">
+>>>>>>> Stashed changes:index.html
 
-   <!--=== CUSTOM STYLES-->
-   <link rel="stylesheet" type="text/css" href="resources/custom/css/custom.css">
+        <!-- Normalize CSS -->
+        <link rel="stylesheet" href="./resources/css/css/normalize.css">
+        <link rel="stylesheet" href="./resources/css/css/main.css">
 
+        <meta name="theme-color" content="#A4CFAB">
 
-</head>
-<body>
-<div class="body-content">
-   <header>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-         <a class="navbar-brand" href="#">Navbar</a>
-         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-         </button>
+        <!-- Bundles CSS only -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+            crossorigin="anonymous">
 
-         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-               <li class="nav-item active">
-                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-               </li>
-               <li class="nav-item">
-                  <a class="nav-link" href="#">Link</a>
-               </li>
-               <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                     Dropdown
-                  </a>
-                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                     <a class="dropdown-item" href="#">Action</a>
-                     <a class="dropdown-item" href="#">Another action</a>
-                     <div class="dropdown-divider"></div>
-                     <a class="dropdown-item" href="#">Something else here</a>
-                  </div>
-               </li>
-               <li class="nav-item">
-                  <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-               </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-               <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-               <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-         </div>
-      </nav>
-   </header>
+        <!-- Custom/Theme Style Sheets Only -->
+        <link rel="stylesheet" href='./resources/css/style.css'>
 
-   <section class="section">
-      <div class="container custom-container">
-         <div class="row">
-            <div class="col-md-12">
+        <!--[if lt IE 9]>
+            <script src="js/vendor/html5-3.6-respond-1.4.2.min.js"></script>
+        <![endif]-->
+    </head>
+    <!--[if lt IE 8]> <p class="browsehappy">You are using an <strong>outdated</strong> browser. 
+        Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p><![endif]-->
+    <body class="">
+        <header></header>
 
+        <section>
+            <div>
+                <nav></nav>
             </div>
-         </div>
-      </div>
-   </section>
+        </section>
 
-   <footer>
 
-   </footer>
-</div>
+        <main class="body-main">
+            <aside></aside>
 
+<<<<<<< Updated upstream:index.php
 <script rel="script" type="text/javascript" src="resources/plugin/js/jquery.min.js"></script>
 <script rel="script" type="text/javascript" src="resources/plugin/js/bootstrap.min.js"></script>
 <script rel="script" type="text/javascript" src="resources/custom/js/main.js"></script>
 </body>
 </html>
+=======
+            <div class="main-content-wrapper">
+                <section>
+                    <h1>Hello World!</h1>
+                </section>
+            </div>
+        </main>
+
+        <!-- JavaScript Bundle with Popper -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+            crossorigin="anonymous"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+        <script>
+            window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+            ga('create', 'UA-XXXXX-Y', 'auto'); ga('set', 'anonymizeIp', true); ga('set', 'transport', 'beacon'); ga('send', 'pageview');
+        </script>
+        <script src="https://www.google-analytics.com/analytics.js" async></script>
+    </body>
+</html>
+>>>>>>> Stashed changes:index.html

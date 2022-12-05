e672a976344896675fb4f6b6a805e2095d5f00d6
Faizan Ahmed <faizanahmed497@gmail.com>
2022-12-05 14:50:40 +0500

    HTML5 SCSS Boilerplate v0.0.1

--- index.php
+++ index.php
@@ -2,104 +2,101 @@
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en" dir="ltr"> <!--<![endif]-->
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document Name | Product Name </title>
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en" dir="ltr">
+<!--<![endif]-->
 
-        <!-- Meta tags to be used for SEO purpose -->
-        <meta name="description" content="">
-        <meta name="keywords" content="">
-        <meta name="author" content="MedCareMSO Development Team">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document Name | Product Name </title>
 
-        <!-- Meta tags to display cards whenever link is shared to any social platform or website -->
-        <!-- Open Graph Meta Data -->
-        <meta property="og:title" content="">
-        <meta property="og:type" content="">
-        <meta property="og:url" content="">
-        <meta property="og:description" content="">
-        <meta property="og:image" content="">
+    <!-- Meta tags to be used for SEO purpose -->
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="MedCareMSO Development Team">
 
-        <meta name="twitter:card" content="">
-        <meta name="twitter:site" content="">
-        <meta name="twitter:title" content="">
-        <meta name="twitter:description" content="">
-        <meta name="twitter:image" content="">
+    <!-- Meta tags to display cards whenever link is shared to any social platform or website -->
+    <!-- Open Graph Meta Data -->
+    <meta property="og:title" content="">
+    <meta property="og:type" content="">
+    <meta property="og:url" content="">
+    <meta property="og:description" content="">
+    <meta property="og:image" content="">
 
-<<<<<<< Updated upstream:index.php
-   <!--=== All Plugins CSS ===-->
-   <link rel="stylesheet" type="text/css" href="resources/plugin/css/bootstrap.min.css">
-=======
-        <link rel="manifest" href="site.webmanifest">
-        <!-- Place favicon.ico in the root directory -->
-        <link rel="apple-touch-icon" href="imgs/favicon.png">
-        <link rel="icon" href="imgs/favicon.png" type="image/x-icon">
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/icon.svg" type="image/svg+xml">
->>>>>>> Stashed changes:index.html
+    <meta name="twitter:card" content="">
+    <meta name="twitter:site" content="">
+    <meta name="twitter:title" content="">
+    <meta name="twitter:description" content="">
+    <meta name="twitter:image" content="">
 
-        <!-- Normalize CSS -->
-        <link rel="stylesheet" href="./resources/css/css/normalize.css">
-        <link rel="stylesheet" href="./resources/css/css/main.css">
+    <link rel="manifest" href="site.webmanifest">
+    <!-- Place favicon.ico in the root directory -->
+    <link rel="apple-touch-icon" href="imgs/favicon.png">
+    <link rel="icon" href="imgs/favicon.png" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" sizes="any">
+    <link rel="icon" href="/icon.svg" type="image/svg+xml">
 
-        <meta name="theme-color" content="#A4CFAB">
+    <!-- Normalize CSS -->
+    <link rel="stylesheet" href="./resources/css/css/normalize.css">
+    <link rel="stylesheet" href="./resources/css/css/main.css">
 
-        <!-- Bundles CSS only -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-            rel="stylesheet"
-            integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
-            crossorigin="anonymous">
+    <meta name="theme-color" content="#A4CFAB">
 
-        <!-- Custom/Theme Style Sheets Only -->
-        <link rel="stylesheet" href='./resources/css/style.css'>
+    <!-- Bundles CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
-        <!--[if lt IE 9]>
+    <!-- Custom/Theme Style Sheets Only -->
+    <link rel="stylesheet" href='./resources/css/style.css'>
+
+    <!--[if lt IE 9]>
             <script src="js/vendor/html5-3.6-respond-1.4.2.min.js"></script>
         <![endif]-->
-    </head>
-    <!--[if lt IE 8]> <p class="browsehappy">You are using an <strong>outdated</strong> browser. 
+</head>
+<!--[if lt IE 8]> <p class="browsehappy">You are using an <strong>outdated</strong> browser. 
         Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p><![endif]-->
-    <body class="">
-        <header></header>
 
-        <section>
-            <div>
-                <nav></nav>
-            </div>
-        </section>
+<body class="">
+    <header></header>
+
+    <section>
+        <div>
+            <nav></nav>
+        </div>
+    </section>
 
 
-        <main class="body-main">
-            <aside></aside>
+    <main class="body-main">
+        <aside></aside>
 
-<<<<<<< Updated upstream:index.php
-<script rel="script" type="text/javascript" src="resources/plugin/js/jquery.min.js"></script>
-<script rel="script" type="text/javascript" src="resources/plugin/js/bootstrap.min.js"></script>
-<script rel="script" type="text/javascript" src="resources/custom/js/main.js"></script>
+        <div class="main-content-wrapper">
+            <section>
+                <h1>Hello World!</h1>
+            </section>
+        </div>
+
+    </main>
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+
+    <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+    <script>
+    window.ga = function() {
+        ga.q.push(arguments)
+    };
+    ga.q = [];
+    ga.l = +new Date;
+    ga('create', 'UA-XXXXX-Y', 'auto');
+    ga('set', 'anonymizeIp', true);
+    ga('set', 'transport', 'beacon');
+    ga('send', 'pageview');
+    </script>
+    <script src="https://www.google-analytics.com/analytics.js" async></script>
 </body>
-</html>
-=======
-            <div class="main-content-wrapper">
-                <section>
-                    <h1>Hello World!</h1>
-                </section>
-            </div>
-        </main>
 
-        <!-- JavaScript Bundle with Popper -->
-        <script
-            src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-            crossorigin="anonymous"></script>
-
-        <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
-        <script>
-            window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
-            ga('create', 'UA-XXXXX-Y', 'auto'); ga('set', 'anonymizeIp', true); ga('set', 'transport', 'beacon'); ga('send', 'pageview');
-        </script>
-        <script src="https://www.google-analytics.com/analytics.js" async></script>
-    </body>
 </html>
->>>>>>> Stashed changes:index.html
